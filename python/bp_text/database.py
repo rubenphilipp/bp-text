fee89a316fcfe0aabc30fdac8a22812cf744b91e
Ruben Philipp <me@rubenphilipp.com>
2025-03-27 00:43:35 +0100

    advanced db access

--- python/bp_text/database.py
+++ python/bp_text/database.py
@@ -5,7 +5,7 @@ from a BibTeX file (as database).
 Created: 2025-03-23
 Author: Ruben Philipp <me@rubenphilipp.com>
 
-$$ Last modified:  00:56:47 Mon Mar 24 2025 CET
+$$ Last modified:  00:42:28 Thu Mar 27 2025 CET
 """
 
 import abc
@@ -28,15 +28,27 @@ class BibTexDatabase(Database):
             split_keywords = True,
             split_files = True):
         self._data = None
-        self.split_keywords = split_keywords  
-        self.split_files = split_files
         if not file_path:
             raise ValueError("File path must be provided.")
         
         self.load(file_path,
-                  split_files = self.split_files,
-                  split_keywords = self.split_keywords)
-            
+                  split_files = split_files,
+                  split_keywords = split_keywords)
+
+    @property
+    def data(self):
+        """Getter for data"""
+        return self._data
+
+    @data.setter
+    def data(self, new_data):
+        self._data = new_data
+
+    @property
+    def entries(self):
+        """Getter for db entries"""
+        return self._data.entries_dict
+
     def load(self,
              file_path:str,
              split_keywords = True,
@@ -46,20 +58,76 @@ class BibTexDatabase(Database):
             print(f"Error: The file {file_path} does not exist.")
             return
         self._data = bibtexparser.parse_file(file_path)
-        for entry in self._data.entries:
-            data_fl = entry.fields_dict.get('file')
-            data_kw = entry.fields_dict.get('keywords')
-            ## split the keywords (by ",") and files (by ";")
-            if(self.split_keywords and data_kw != None):
-                entry['keywords'] = data_kw.value.split(",")
-            if(self.split_files and data_fl != None):
-                entry['files'] = data_fl.value.split(";")
+        if split_keywords:
+            self.split_fields_by(field = "keywords", separator=",")
+        if split_files:
+            self.split_fields_by(field = "file", separator=";")
         return self._data
 
-    def get_nth_entry(self, n):
-        return self._data.entries[n]
+    def split_fields_by(self,
+                        field: str,
+                        separator = ";"):
+        """
+        Splits the data/value of all entries in the database (destructively)
+        of the given field (e.g. "keywords") by a given separator.
+        """
+        ## sanity checks
+        if not field:
+            raise ValueError("A field must be provided.")
+        if not separator:
+            raise ValueError("A seperator must be provided.")
+        for entry_key, entry in self.data.entries_dict.items():
+            field_data = entry.fields_dict.get(field)
+            if field_data:
+                field_value = field_data.value
+                if isinstance(field_value, str):
+                    field_data.value = field_value.split(separator)
+                else:
+                    print(f"Field '{field}' is already a list and thus cannot "
+                          + "be split. ")
+        return self.data
+
+    def get_entry_by_key(self, key):
+        """
+        Getter for a specific entry by citation key in the db.
+        """
+        return self.entries.get(key)
+
+    def find_entries(self, field: str, search: str):
+        """
+        Find entries matching the search string in the given field.
+
+        Return: A list with items of class <class 'bibtexparser.model.Entry'>
+        """
+        matches = []
+
+        for entr in self.entries:
+            entry = self.entries[entr]
+            if field in entry.fields_dict:
+                value = entry.get(field)
+
+                # If the value is a list, check if search is in the list
+                if isinstance(value, list):
+                    if search.lower() in [str(item).lower() for item in value]:
+                        matches.append(entry)
+                # If the value is not a list, do a standard string comparison
+                else:
+                    if search.lower() in str(value).lower():
+                        matches.append(entry)
+
+        return matches
         
+
     
+
+    def get_nth_entry(self, n):
+        entries = self.data.entries
+        if (n < len(entries)):
+            return entries[n]
+        else:
+            print(f"Entry '{n}' does is not within the list range.")
+        
+
 
 ################################################################################
 ## EOF db.py
