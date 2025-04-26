5f672628ab00771e08c23e00aad705698e738180
Ruben Philipp <me@rubenphilipp.com>
2025-04-26 02:38:05 +0200

    tests

--- python/tests/test_db.py
+++ python/tests/test_db.py
@@ -1,7 +1,10 @@
 import unittest
 import os
+import shutil
+from pathlib import Path
 
 from bp_text import database
+from bp_text import pool
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(TEST_DIR, 'data')
@@ -22,3 +25,16 @@ class TestDatabase(unittest.TestCase):
     def test_file_split(self):
         result = self.db1.entries["pfeifer2003"].get("file").value
         self.assertEqual(len(result), 2)
+
+    def test_make_pool(self):
+        dir_path = Path("/tmp/test_pool_cache")
+        if dir_path.exists() and dir_path.is_dir():
+            shutil.rmtree(dir_path)
+            
+        # new cache
+        result1 = self.db1.make_pool(cache="/tmp/test_pool_cache")
+        # existing cache
+        result2 = self.db1.make_pool(cache="/tmp/test_pool_cache")
+        self.assertEqual(isinstance(result1, pool.Pool), True)
+        self.assertEqual(isinstance(result2, pool.Pool), True)
+        
