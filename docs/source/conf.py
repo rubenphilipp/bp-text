6051f766c17aeb3ab1e4535d8ad943095aadad44
Ruben Philipp <me@rubenphilipp.com>
2025-04-30 10:42:17 +0200

    doc's version/release is now linked to __init__.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,13 +3,41 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import re
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../bp_text'))
+
+autosummary_generate = True
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': False,
+    'special-members': '__init__',
+    'inherited-members': True,
+    'show-inheritance': True,
+}
+
+## get version
+def get_version():
+    here = os.path.abspath(os.path.dirname(__file__))  # path to conf.py
+    init_path = os.path.join(here, '..', '..', 'bp_text', '__init__.py')  
+    with open(init_path, "r") as f:
+        content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+################################################################################
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'bp_text'
 copyright = '2025, Ruben Philipp'
 author = 'Ruben Philipp'
-release = '0.0.1'
+release = get_version()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -34,16 +62,3 @@ html_static_path = ['_static']
 
 ################################################################################
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../bp_text'))
-
-autosummary_generate = True
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'private-members': False,
-    'special-members': '__init__',
-    'inherited-members': True,
-    'show-inheritance': True,
-}
