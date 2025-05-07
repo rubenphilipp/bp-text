6e7121b9272feb4f038045d4f3bfed49dc3a2fdd
Ruben Philipp <me@rubenphilipp.com>
2025-05-07 15:11:42 +0200

    first draft of org-mode export

--- bp_text/textfragment.py
+++ bp_text/textfragment.py
@@ -8,9 +8,11 @@ from the `meta` slot of a :py:class:`Pool` instance.
 Created: 2025-05-06
 Author: Ruben Philipp <me@rubenphilipp.com>
 
-$$ Last modified:  01:14:41 Tue May  6 2025 CEST
+$$ Last modified:  15:11:10 Wed May  7 2025 CEST
 
 """
+
+from spacy import tokens
 
 ################################################################################
 
@@ -98,6 +100,91 @@ class TextFragment:
         self._data = val
 
     ########################################
+
+    def format_org(self,
+                   cite = True,
+                   force_cite = False):
+        """This returns a formatted string with the text contained in `data` in
+        org-mode syntax.
+
+        :param cite: When True, an org-cite citation will be appended to the
+            generated string. Default = True
+        :type cite: boolean
+        :param force_cite: When True (and `cite`=True), also for a citation for
+            text elements that normally don't "require" a citation (e.g. PUNCT).
+            Default = False
+        :type force_cite: boolean
+
+        :returns: A string with the content of `text` formatted in org-mode
+            syntax. 
+        """
+        res = ""
+        res_pos = None
+
+        if isinstance(self.data, str):
+            res = self.data
+        elif isinstance(self.data, tokens.Token):
+            res = self.data.text
+            res_pos = self.data.pos_
+        else:
+            print("TextFragment.format_org: The data does not contain valid "
+                  + "text.")
+            return None
+
+        if (cite and self.key
+            and ((res_pos != "PUNCT" and res_pos != "SPACE")
+                 or force_cite)):
+            if self.page_label:
+                res += "[cite:@" + self.key + " " + self.page_label + "]"
+            else:
+                res += "[cite:@" + self.key + "]"
+
+        return res
+
+
+################################################################################
+
+def textfragments_to_org(fragment_list,
+                         cite = True,
+                         force_cite = False):
+    """This function takes a `list` of :py:class:`TextFragment` objects and
+    returns a `string` formatted in org-mode syntax, (optionally) including
+    org-cite references.
+
+    :param fragment_list: The :py:class:`TextFragment` objects. 
+    :type fragment_list: A list with :py:class:`TextFragment` objects.
+    :param cite: When True, an org-cite citation will be appended to the
+        generated string. Default = True
+    :type cite: boolean
+    :param force_cite: When True (and `cite`=True), also for a citation for
+            text elements that normally don't "require" a citation (e.g. PUNC).
+            Default = False
+    :type force_cite: boolean
+    """
+    if not (isinstance(fragment_list, list)
+            and all(isinstance(ele, TextFragment) for ele in fragment_list)):
+        raise ValueError("textfragments_to_org: fragment_list must be a list "
+                         + "of TextFragment objects.")
+
+    res_string = ""
+    for i, frag in enumerate(fragment_list):
+        res_string += frag.format_org(cite = cite,
+                                      force_cite = force_cite)
+        ## add a space if this is not the last fragment and the next fragment's
+        ## token is not punctuation
+        ## always adds a space if the data contains something else than a
+        ## spacy.Token 
+        if i < len(fragment_list) - 1:
+            next_frag = fragment_list[i + 1]
+            if (isinstance(next_frag.data, tokens.Token)
+                and isinstance(frag.data, tokens.Token)):
+                if not next_frag.data.is_punct and not frag.data.is_space:
+                    res_string += " "
+            else:
+                res_string += " "
+
+    return res_string
+        
 
 ################################################################################
 ## EOF textfragment.py
